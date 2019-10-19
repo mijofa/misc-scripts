b3afb784725954a031d7710a8e873fd13cf03f56
Mike Abrahall <mike@abrahall.id.au>
2019-10-19 15:45:31 +1100

    Update to handle multiple file arguments at once

--- extract.sh
+++ extract.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
+set -eEu -o pipefail
+shopt -s failglob
+trap 'echo >&2 "${BASH_SOURCE:-$0}:${LINENO}: unknown error"' ERR
 
-tmp_extraction=$(mktemp --tmpdir="$PWD" --directory)
 
-archive="${@: -1}"  # Last argument
-new_dir="${archive%.*}"  # Remove everything after the last .
-new_dir="${new_dir%.tar}"  # Remove .tar from the end (for cases such as .tar.gz)
+actually_do_extraction () {
+    tmp_extraction=$(mktemp --tmpdir="$PWD" --directory)
 
-if [ -d "$new_dir" ] ; then
-    echo "ERROR: $new_dir already exists, stopping to avoid possible overwrite"
-    exit 2
-fi
+    archive="$1"
+    new_dir="${archive%.*}"  # Remove everything after the last .
+    new_dir="${new_dir%.tar}"  # Remove .tar from the end (for cases such as .tar.gz)
 
-echo "Extracting $archive"
-file-roller --extract-to="$tmp_extraction" "$@"
+    if [ -d "$new_dir" ] ; then
+        echo "ERROR: $new_dir already exists, stopping to avoid possible overwrite"
+        exit 2
+    fi
 
-extraction_count=$(find "$tmp_extraction" -mindepth 1 -maxdepth 1 | wc -l)
-if [ "$extraction_count" == 1 ] ; then
-    # FIXME: Should I just use "mv $tmp_extraction/* ."?
-    #        I'm a bit nervous about . files or similar
-    contents="$(find "$tmp_extraction" -mindepth 1 -maxdepth 1)"
-    mv "$contents" "$PWD"
-    echo "Extracted ${contents##*/}"
-    rmdir "$tmp_extraction"
-elif [ "$extraction_count" != 0 ] ; then
-    mv "$tmp_extraction" "$new_dir"
-    echo "Extracted contents to $new_dir"
-else
-    echo "ERROR: No contents found!" >&2
-    rmdir "$tmp_extraction"
-    exit 2
-fi
+    echo "Extracting $archive"
+    file-roller --extract-to="$tmp_extraction" "$@"
+
+    extraction_count=$(find "$tmp_extraction" -mindepth 1 -maxdepth 1 | wc -l)
+    if [ "$extraction_count" == 1 ] ; then
+        # FIXME: Should I just use "mv $tmp_extraction/* ."?
+        #        I'm a bit nervous about . files or similar
+        contents="$(find "$tmp_extraction" -mindepth 1 -maxdepth 1)"
+        mv "$contents" "$PWD"
+        echo "Extracted ${contents##*/}"
+        rmdir "$tmp_extraction"
+    elif [ "$extraction_count" != 0 ] ; then
+        mv "$tmp_extraction" "$new_dir"
+        echo "Extracted contents to $new_dir"
+    else
+        echo "ERROR: No contents found for $archive!" >&2
+        rmdir "$tmp_extraction"
+        exit 2
+    fi
+}
+
+for f in "$@" ; do
+    # FIXME: Run them in parallel then wait for all of them to finish?
+    actually_do_extraction "$f"
+done
