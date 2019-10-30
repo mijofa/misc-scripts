38ff53ebb1ca0178a9ac1458259b82c964436cd6
Mike Abrahall <mike@cyber.com.au>
2019-10-30 11:42:59 +1100

    Use ix.io instead of sprunge.sh. Try to fix the history grabber thing, added a FIXME

--- sprunge.sh
+++ sprunge.sh
@@ -2,12 +2,13 @@
 {
     cat ${1:-/dev/stdin} # If a filename is specified as the first argument, use that, otherwise use stdin.
 } | {
-    url="$(curl --silent --show-error -F 'sprunge=<-' http://sprunge.us)"
+    url="$(curl --silent --show-error -F 'f:1=<-' http://ix.io)"
 
     if   [ -n "$1" ] ; then
         str="$1: $url"
-    elif [ -z "$1" ] && history 1| grep -q 'sprunge' ; then
-        str="$ $(history 1|sed -e 's/[[:digit:]]\+\s\+//') # $url" # This grabs the last line in the history which should be the current command you are running. The sed expression will probably not work as expected if you've customized the history format.
+## FIXME: This fc thing doesn't even work because the subshell doesn't have the history from the parent shell
+#    elif [ -z "$1" ] && fc -nl -0 | grep -q "$0" ; then
+#        str="$ $(fc -nl -0|sed -e 's/^\s\+//') # $url" # This grabs the last line in the history which should be the current command you are running. The sed expression will probably not work as expected if you've customized the history format.
     else
         str="$url"
     fi
@@ -16,7 +17,7 @@
     if [ -t 1 ] && [ -n "$DISPLAY" ] ; then
         # If stdout is not a terminal then we're probably being piped into something
         # If DISPLAY is not set this won't work anyway
-	# Otherwise put the sprunge URL in the X copy-paste buffer.
+        # Otherwise put the sprunge URL in the X copy-paste buffer.
 
         # The 3 different selection buffers confuse me, so use all of them.
         # I use nohup here because xclip never actually dies it just orphans itself, although not enough to be able to close a screen window.
