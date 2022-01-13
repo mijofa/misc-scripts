07d1493a9fa4b9603a41b4f2122ec0b7977e40df
Mike Abrahall <mike@abrahall.id.au>
2022-01-13 17:30:35 +1100

    Switch to only monitoring the fallback/default sink & source, rather than *every* sink & source

--- pa-vol-notifier.py
+++ pa-vol-notifier.py
@@ -81,31 +81,21 @@ class NotificationController(object):
 class DeviceHandler(object):
     """Handle device specific D-Bus signals from PulseAudio."""
 
-    def __init__(self, pulse_bus, dev_path, notifier):
+    def __init__(self, pulse_bus, dev_path, dev_type):
         """Set up the listeners."""
         self._path = dev_path
 
         self.device = pulse_bus.get_object("org.PulseAudio.Core1.Device", dev_path)
-
-        # Is this even a device we should be monitoring
-        if not bool(self.device.Get("org.PulseAudio.Core1.Device", "HasHardwareVolume")) and \
-                not bool(self.device.Get("org.PulseAudio.Core1.Device", "HasHardwareMute")):
-            # FIXME: This should probably raise an exception that gets handled
-            self.device = None
-            return None
 
         # Prepopulate the initial state
         self._MuteUpdated(self.device.Get("org.PulseAudio.Core1.Device", "Mute"), quiet=True)
         self._VolumeUpdated(self.device.Get("org.PulseAudio.Core1.Device", "Volume"), quiet=True)
 
         # Set up relevant event listeners
-        # if bool(self.device.Get("org.PulseAudio.Core1.Device", "HasHardwareMute")):
         pulse_bus.add_signal_receiver(handler_function=self._MuteUpdated, signal_name='MuteUpdated', path=dev_path)
-        # if bool(self.device.Get("org.PulseAudio.Core1.Device", "HasHardwareVolume")):
         pulse_bus.add_signal_receiver(handler_function=self._VolumeUpdated, signal_name='VolumeUpdated', path=dev_path)
 
-#        self.notifier = NotificationController(icons[dev_type])
-        self.notifier = notifier
+        self.notifier = NotificationController(icons[dev_type])
         self.pulse_bus = pulse_bus
 
     def _MuteUpdated(self, new_mute, quiet=False):
@@ -128,15 +118,18 @@ class DeviceHandler(object):
     def close(self):
         """Close the device handler."""
         if self.device:
+            print("Closing device", self._path)
+            # FIXME: None of this actually stops anything from happening
             self.pulse_bus.remove_signal_receiver(self._VolumeUpdated)
             self.pulse_bus.remove_signal_receiver(self._MuteUpdated)
-#            self.notifier.close()
+            self.notifier.close()
 
 
 class PulseHandler(object):
     """Handle D-Bus signals from PulseAudio."""
 
-    devices = {}
+    _sink = None
+    _source = None
 
     def _get_bus_address(self):
         if 'PULSE_DBUS_SERVER' in os.environ:
@@ -148,60 +141,62 @@ class PulseHandler(object):
                                         dbus_interface="org.freedesktop.DBus.Properties")
         return address
 
-    def __init__(self, bus_address=None):
+    def __init__(self, loop, bus_address=None):
         """Set up D-Bus listeners."""
+        self.loop = loop
+
         if not bus_address:
             bus_address = self._get_bus_address()
         self.pulse_bus = dbus.connection.Connection(bus_address)
         pulse_core = self.pulse_bus.get_object(object_path='/org/pulseaudio/core1')
-        for signal in ("NewSink", "SinkRemoved", "NewSource", "SourceRemoved", 'Device.MuteUpdated', 'Device.VolumeUpdated'):
+        # for signal in ("NewSink", "SinkRemoved", "NewSource", "SourceRemoved", 'Device.MuteUpdated', 'Device.VolumeUpdated'):
+        for signal in ("FallbackSinkUpdated", "FallbackSourceUpdated",
+                       "FallbackSinkUnset", "FallbackSourceUnset",
+                       "Device.MuteUpdated", "Device.VolumeUpdated"):
             pulse_core.ListenForSignal(f'org.PulseAudio.Core1.{signal}',
                                        dbus.Array(signature='o'),
                                        dbus_interface='org.PulseAudio.Core1')
 
-        self.input_notifier = NotificationController(icons['source'])
-        self.output_notifier = NotificationController(icons['sink'])
+        self.change_sink(pulse_core.Get("org.PulseAudio.Core1", "FallbackSink"))
+        self.change_source(pulse_core.Get("org.PulseAudio.Core1", "FallbackSource"))
 
-        # Prepulate with the devices already connected
-        for dev_path in pulse_core.Get("org.PulseAudio.Core1", "Sinks"):
-            self.add_sink(dev_path)
-            # self.devices[dev_path] = DeviceHandler(self.pulse_bus, dev_path, dev_type='sink')
-        for dev_path in pulse_core.Get("org.PulseAudio.Core1", "Sources"):
-            self.add_source(dev_path)  # DeviceHandler(self.pulse_bus, dev_path, dev_type='source')
+        self.pulse_bus.add_signal_receiver(handler_function=self.change_sink, signal_name='FallbackSinkUpdated')
+        self.pulse_bus.add_signal_receiver(handler_function=self.change_source, signal_name='FallbackSourceUpdated')
+        # FIXME: These don't seem to fire at all
+        self.pulse_bus.add_signal_receiver(handler_function=self.change_sink, signal_name='FallbackSinkUnset')
+        self.pulse_bus.add_signal_receiver(handler_function=self.change_source, signal_name='FallbackSourceUnset')
 
-        # FIXME: Add a signal handler for new sinks/sources, create new listeners for each
-        #         def f(*args, **kwargs):
-        #             print('device', args, kwargs)
-        #         self.pulse_bus.add_signal_receiver(f, 'DeviceUpdated')
+    def change_sink(self, sink_path=None):
+        """Change what sink device we're monitoring."""
+        print("Changing sink", sink_path)
+        if self._sink:
+            self._sink.close()
+            self._sink = None
 
-        # FIXME: Not working
-        self.pulse_bus.add_signal_receiver(handler_function=self.add_sink, signal_name='NewSink')
-        self.pulse_bus.add_signal_receiver(handler_function=self.add_source, signal_name='NewSource')
-        self.pulse_bus.add_signal_receiver(handler_function=self.remove_device, signal_name='SinkRemoved')
-        self.pulse_bus.add_signal_receiver(handler_function=self.remove_device, signal_name='SourceRemoved')
+            # FIXME: Since this doesn't actually work, I'm just going to exit and let Systemd take care of it
+            self.loop.quit()
+        if sink_path:
+            self._sink = DeviceHandler(self.pulse_bus, sink_path, dev_type='sink')
 
-    def add_sink(self, sink_path):
-        """Add new sink device handler."""
-        print("Adding sink", sink_path)
-        assert sink_path not in self.devices
-        self.devices[sink_path] = DeviceHandler(self.pulse_bus, sink_path, notifier=self.output_notifier)
+    def change_source(self, source_path=None):
+        """Change what source device we're monitoring."""
+        print("Changing source", source_path)
+        if self._source:
+            self._source.close()
+            self._source = None
 
-    def add_source(self, source_path):
-        """Add new source device handler."""
-        print("Adding source", source_path)
-        assert source_path not in self.devices
-        self.devices[source_path] = DeviceHandler(self.pulse_bus, source_path, notifier=self.input_notifier)
-
-    def remove_device(self, dev_path):
-        """Remove device handler."""
-        print("Removing dev", dev_path)
-        assert dev_path in self.devices
-        self.devices.pop(dev_path).close()
+            # FIXME: Since this doesn't actually work, I'm just going to exit and let Systemd take care of it
+            self.loop.quit()
+        if source_path:
+            self._source = DeviceHandler(self.pulse_bus, source_path, dev_type='source')
 
 
 dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 Notify.init(sys.argv[0])
 
-pulse = PulseHandler()
+loop = GLib.MainLoop()
+pulse = PulseHandler(loop)
+loop.run()
 
-GLib.MainLoop().run()
+# Should never really make it here, so exit with an error code if we do
+exit(1)
