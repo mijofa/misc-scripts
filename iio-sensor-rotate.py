f5c46b9d1e3be140fb22a3c2ddf400ed56c2e17b
Mike Abrahall <mike@cyber.com.au>
2024-05-16 14:42:03 +1000

    Switch to dbus instead of subprocess for iio-sensor

--- iio-sensor-rotate.py
+++ iio-sensor-rotate.py
@@ -46,6 +46,14 @@ index 0000000..db6b5bd
 import subprocess
 import os
 
+import dbus
+import dbus.mainloop.glib
+import dbus.service
+from gi.repository import GLib
+from gi.repository import Gio
+
+dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
 ROTATION_MAP = {
     'normal': 'normal',
     'bottom-up': 'inverted',
@@ -53,26 +61,99 @@ ROTATION_MAP = {
     'right-up': 'right',
 }
 
+
+class RotationDaemon(dbus.service.Object):
+    """Watch iio-sensor-proxy for accelerometer updates."""
+
+    def __init__(self, main_loop: GLib.MainLoop):
+        self.main_loop: GLib.MainLoop = main_loop
+        # get buses
+        self.bus = dbus.SystemBus()
+
+        # subscribe to sensor
+        self.sensor_proxy = self.bus.get_object('net.hadess.SensorProxy', '/net/hadess/SensorProxy')
+        self.sensor_interface = dbus.Interface(self.sensor_proxy, dbus_interface='net.hadess.SensorProxy')
+        self.sensor_interface.ClaimAccelerometer()
+        self.sensor_proxy.connect_to_signal("PropertiesChanged", dbus_interface="org.freedesktop.DBus.Properties", handler_function=self.accelerometer_changed_handler)
+
+    def accelerometer_changed_handler(self, _proxy, args, _signature):
+        match args['AccelerometerOrientation']:
+            case 'normal':
+                x11_rotation = 'normal'
+            case 'bottom-up':
+                x11_rotation = 'inverted'
+            case 'left-up':
+                x11_rotation = 'left'
+            case 'right-up':
+                x11_rotation = 'right'
+            case _:
+                raise NotImplementedError(f"Unrecognised rotation value: {args['AccelerometerOrientation']}")
+        
+        self.x11_rotate(x11_rotation)
+
+    def x11_rotate(self, rotation: str):
+        return subprocess.check_call(args=['xrandr',
+                                           '--output', 'eDP-1',
+                                           '--rotation', rotation])
+
+
+class SubprocDaemon():
+    def __init__(self, args: list[str]):
+        GLib.idle_add(self.run, args)
+
+    def run(self, args: list[str]):
+        self.cancellable = Gio.Cancellable()
+        flags = Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_MERGE
+        self.process = p = Gio.Subprocess.new(args, flags)
+        p.wait_check_async(
+            cancellable=self.cancellable,
+            callback=self._on_finished
+        )
+        stream = p.get_stdout_pipe()
+        self.data_stream = Gio.DataInputStream.new(stream)
+        self.queue_read()
+
+    def queue_read(self):
+        self.data_stream.read_line_async(
+            io_priority=GLib.PRIORITY_DEFAULT,
+            cancellable=self.cancellable,
+            callback=self._on_data
+        )
+
+    def cancel_read(self):
+        self.cancellable.cancel()
+
+    def _on_finished(self, proc, results):
+        proc.wait_check_finish(results)
+        self.cancel_read()
+
+    def _on_data(self, source, result):
+        line, length = source.read_line_finish_utf8(result)
+        if line:
+            event, _, __, state = line.split()
+            if event == 'video/tabletmode':
+                if int(state) == 0:
+                    print('laptop mode')
+                elif int(state) == 0:
+                    print('tablet mode')
+                else:
+                    raise NotImplementedError(f"Unknown ACPI state for {event}: {state}")
+        self.queue_read()
+
+    def stop(self):
+        self.process.send_signal(signal.SIGTERM)
+
+    def kill(self):
+        self.cancel_read()
+        self.process.send_signal(signal.SIGKILL)
+
+
+
 if __name__ == '__main__':
     if 'DISPLAY' not in os.environ:
         raise Exception("Need an X11 session to work with")
 
-    # FIXME: monitor-sensor is just a DBUS client,
-    #        figure that shit out and use DBUS directly
-    monitor_sensor = subprocess.Popen(args=['monitor-sensor'],
-                                      stdout=subprocess.PIPE,
-                                      text=True)
-    while line := monitor_sensor.stdout.readline():
-        if line.strip().startswith('Accelerometer orientation changed: '):
-            new_rotation = line.strip().rsplit(maxsplit=1)[-1]
-            subprocess.check_output(args=['xrandr',
-                                          '--output', 'eDP-1',
-                                          '--rotation', ROTATION_MAP[new_rotation]])
-        elif line.strip().startswith('=== Has accelerometer (orientation:'):
-            init_rotation = line.strip().rsplit(maxsplit=1)[-1].strip(')')
-            if init_rotation == 'undefined':
-                # Sometimes (most of the time) it takes a moment after initialisation for the accelerometer to provide useful info
-                continue
-            subprocess.check_output(args=['xrandr',
-                                          '--output', 'eDP-1',
-                                          '--rotation', ROTATION_MAP[init_rotation]])
+    loop = GLib.MainLoop()
+    iio_daemon = RotationDaemon(main_loop=loop)
+    # tablet_flap_daemon = SubprocDaemon(args=['acpi_listen'])
+    loop.run()
