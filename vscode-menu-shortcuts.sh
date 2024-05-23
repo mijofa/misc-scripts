a195c3f950b2e67f6a12fb08a5dfff6ae59d601b
Mike Abrahall <mike@abrahall.id.au>
2024-05-23 16:14:03 +1000

    Added VScode workspace menu shortcuts

---
+++ vscode-menu-shortcuts.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This just puts a menu item in for each VScode workspace I use regularly.
+
+# This is kinda silly to have a script, but I expect to rerun it every now and then.
+# And I'll kinda want it on multiple different systems, so script was the quickest answer.
+# NOTE: I considered having a separate git repo of .code-workspace files, but I still couldn't get that into my menu
+
+
+# ~/vcs/{abrahall.id.au,cyber/prisonpc,PI/ichris-sync,PI/ansible-playbooks,misc-scripts}
+for project_path in "$@" ; do
+    project="${project_path/\/home\/mike\/vcs\//}"
+    project_shortcut="code-${project//\//_}.desktop"
+    sed -e '/^\[Desktop Action/,$d' \
+        -e '/^Actions=/d' \
+        -e '/^GenericName=/d' \
+        -e '/^Categories=/ s/=.*$/=X-mijofa-code-projects;/' \
+        -e '/^Name=/ s/=.*$/='"${project//\//\\/}"'/' \
+        -e '/^Comment=/ s/=.*$/='"${project_path//\//\\/}"'/' \
+        -e '/^Exec=/ s/%F/"'"${project_path//\//\\/}"'"/g' \
+        /usr/share/applications/code.desktop >~/.local/share/applications/"$project_shortcut"
+done
+
+printf >~/.local/share/desktop-directories/code-projects.directory '%s\n' \
+       "[Desktop Entry]" \
+       "Version=1.0" \
+       "Type=Directory" \
+       "Name=Code projects" \
+       "Icon=vscode" \
+
+printf >~/.config/menus/applications-merged/code-projects.menu '%s\n'  \
+       "<Menu>" \
+       "    <Name>Applications</Name>" \
+       "    <Menu>" \
+       "        <Name>Code Projects</Name>" \
+       "        <Directory>code-projects.directory</Directory>" \
+       "        <Include>" \
+       "            <Category>X-mijofa-code-projects</Category>" \
+       "        </Include>" \
+       "    </Menu>" \
+       "</Menu>"
